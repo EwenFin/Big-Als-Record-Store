36cbff81534b0d6f533c1287e855d88ad013dc6d
Ewen Carr <ewencarr@hotmail.com>
2017-02-24 16:25:48 +0000

    added album/show.erb and get method

--- controllers/album_controller.rb
+++ controllers/album_controller.rb
@@ -2,7 +2,16 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/album.rb' )
 
-get'/albums' do
+get '/albums' do
   @albums = Album.all
-  erb ( :"albums/index")
+  erb(:"albums/index")
+end
+
+get '/albums/new' do
+  erb(:"albums/new")
+end
+
+get '/albums/:id' do
+  @album = Album.find_album_by_id(params[:id])
+  erb(:"albums/show")
 end

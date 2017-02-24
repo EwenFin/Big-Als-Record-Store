2e8138792ff7061d7dbcf9005a41fcfea44bffbb
Ewen Carr <ewencarr@hotmail.com>
2017-02-24 10:35:15 +0000

    added shop class and spec files

---
+++ models/shop.rb
@@ -0,0 +1,21 @@
+require_relative ('../db/sql_runner.rb')
+
+class Shop
+  attr_reader :till
+  attr_accessor :stock_array
+  
+  def initialize(till, stock_array)
+    #Are these accessors redundant?  
+    @till = 0
+    @stock_array = []
+  end
+
+  def add_to_stock(album)
+    @stock_array << album
+  end
+
+  def remove_from_stock(album)
+    @stock_array.delete(album)
+  end
+
+end
---
+++ models/specs/shop_spec.rb
@@ -0,0 +1,52 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../shop.rb')
+require_relative('../album.rb')
+require_relative('../artist.rb')
+
+class TestShop < MiniTest::Test
+  def setup
+    @artist1 = Artist.new({'name' => 'Miles Davis'})
+  
+    @artist2 = Artist.new({'name' => 'Michael Jackson'})
+    
+    @album1 = Album.new({
+      'title' => 'Blue in Green',
+      'genre' => 'Jazz',
+      'quantity' => 5,
+      'retail' => 9.99,
+      'wholesale' => 5.34,
+      'artist_id' => @artist1.id,
+      })
+
+    @album2 = Album.new({
+      'title' => 'Thriller',
+      'genre' => 'Pop',
+      'quantity' => 9,
+      'retail' => 7.99,
+      'wholesale' => 5.04,
+      'artist_id' => @artist2.id,
+      })
+
+    @shop1 = Shop.new(0,[])
+  end
+
+  def test_shop_has_stock
+    @shop1.add_to_stock(@album1)
+    @shop1.add_to_stock(@album2)
+    assert_equal([@album1, @album2], @shop1.stock_array)
+  end
+
+  def test_shop_can_remove_stock
+    @shop1.add_to_stock(@album1)
+    @shop1.add_to_stock(@album2)
+    assert_equal([@album1, @album2], @shop1.stock_array)
+    @shop1.remove_from_stock(@album1)
+    assert_equal([@album2], @shop1.stock_array)
+  end
+
+
+
+
+  
+end

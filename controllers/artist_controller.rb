5a5be39104ba285fc90f39a8c8f59b22009a69ac
Ewen Carr <ewencarr@hotmail.com>
2017-02-25 13:26:11 +0000

    added new and id routes to artist_controller and relevant .erb files

--- controllers/artist_controller.rb
+++ controllers/artist_controller.rb
@@ -6,3 +6,13 @@ get '/artists' do
   @artist = Artist.all
   erb ( :"artists/index")
 end
+
+
+get '/artists/new' do
+  erb(:"artists/new")
+end
+
+get '/artists/:id' do
+  @artist = Artist.find_artist_by_id(params[:id])
+  erb(:"artists/show")
+end
